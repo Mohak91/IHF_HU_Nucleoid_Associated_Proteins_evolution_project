0b5eb79f8a6f9371fd21eb90a9f77ad427e24b18
Mohak Sharda <sharda.mohak@gmail.com>
2022-08-12 15:22:54 +0530

    Update prepare_files_for_msa.py

--- prepare_files_for_msa.py
+++ prepare_files_for_msa.py
@@ -4,10 +4,24 @@ import pandas as pd
 
 #reading csv file input
 
-FILENAME_CSV = "/Users/User/Documents/ihf_hu_project/ihf_hu_project/data/HUA_1030198_bacterial_protein_id_nums_only.csv"
-PREFIX = "HU_alpha"
-FASTA = "/Users/User/Documents/ihf_hu_project/ihf_hu_project/data/HUA_1030198_all_390_members_including_bacteria.fasta"
-WRITE="/Users/User/Documents/ihf_hu_project/ihf_hu_project/results/HUA_1030198_bacteria_nr_species.fasta"
+FILENAME_CSV = ["/Users/User/Documents/ihf_hu_project/ihf_hu_project/data/HUA_1030198_bacterial_protein_id_nums_only.csv",
+                "/Users/User/Documents/ihf_hu_project/ihf_hu_project/data/HUB_1030195_bacterial_protein_id_nums_only.csv",
+                "/Users/User/Documents/ihf_hu_project/ihf_hu_project/data/IHFA_1030196_bacterial_protein_id_nums_only.csv",
+                "/Users/User/Documents/ihf_hu_project/ihf_hu_project/data/IHFB_1030197_bacterial_protein_id_nums_only.csv"]
+
+PREFIX = ["HU_alpha","HU_beta","IHF_alpha","IHF_beta"]
+
+FASTA = ["/Users/User/Documents/ihf_hu_project/ihf_hu_project/data/HUA_1030198_all_390_members_including_bacteria.fasta",
+         "/Users/User/Documents/ihf_hu_project/ihf_hu_project/data/HUB_1030195_all_993_members_including_bacteria.fasta",
+         "/Users/User/Documents/ihf_hu_project/ihf_hu_project/data/IHFA_1030196_all_766_members_including_bacteria.fasta",
+         "/Users/User/Documents/ihf_hu_project/ihf_hu_project/data/IHFB_1030197_all_607_members_including_bacteria.fasta"]
+
+WRITE=["/Users/User/Documents/ihf_hu_project/ihf_hu_project/results/HUA_1030198_bacteria_nr_species.fasta",
+       "/Users/User/Documents/ihf_hu_project/ihf_hu_project/results/HUB_1030195_bacteria_nr_species.fasta",
+       "/Users/User/Documents/ihf_hu_project/ihf_hu_project/results/IHFA_1030196_bacteria_nr_species.fasta",
+       "/Users/User/Documents/ihf_hu_project/ihf_hu_project/results/IHFB_1030197_bacteria_nr_species.fasta"]
+
+MERGED_FASTA="/Users/User/Documents/ihf_hu_project/ihf_hu_project/results/hua_hub_ihfa_ihfb_merged_bacteria_nr_species.fasta"
 
 __prog_name__ = 'prepare_files_for_msa.py'
 __prog_desc__ = 'Takes a csv, fasta and output file name as input and filters non-redundant species level protein sequences in a fasta format.'
@@ -28,22 +42,24 @@ class PrepareForMsa(object):
         pass
 
     def main(self):
-        protid_list = self.make_protid_list(FILENAME_CSV)
-        fasta_dict = self.read_fasta(FASTA)
-        filtered_bacterial_fasta_dictionary = self.filter_bacteria_prot_sequences(protid_list,fasta_dict)
-        filtered_bacterial_fasta_dataframe = self.remove_species_level_redundancy(filtered_bacterial_fasta_dictionary)
-        self.write_filtered_fasta_file(filtered_bacterial_fasta_dataframe)
+        for filename_csv,prefix,fasta,write in zip(FILENAME_CSV,PREFIX,FASTA,WRITE):
+            protid_list = self.make_protid_list(filename_csv)
+            fasta_dict = self.read_fasta(fasta)
+            filtered_bacterial_fasta_dictionary = self.filter_bacteria_prot_sequences(protid_list,fasta_dict)
+            filtered_bacterial_fasta_dataframe = self.remove_species_level_redundancy(filtered_bacterial_fasta_dictionary,prefix)
+            self.write_filtered_fasta_file(filtered_bacterial_fasta_dataframe,write)
+        self.write_merged_filtered_fasta_file(WRITE,MERGED_FASTA)
     
-    def make_protid_list(self,FILENAME_CSV):
-        csv_bacteria = pd.read_csv(FILENAME_CSV)
+    def make_protid_list(self,filename_csv):
+        csv_bacteria = pd.read_csv(filename_csv)
         protid_list = csv_bacteria['Protein ID'].tolist()
         
         return protid_list
 
     #reading fasta file input
-    def read_fasta(self,FASTA):
+    def read_fasta(self,fasta):
         fasta_dict={}
-        with open(FASTA) as fh:
+        with open(fasta) as fh:
             seq=""
             for line in fh:
                 if line.startswith(">"):
@@ -55,6 +71,7 @@ class PrepareForMsa(object):
                     fasta_dict[protein_id]=[seq,organism_name]
                     seq=""
         return fasta_dict
+    
     def filter_bacteria_prot_sequences(self,taxon_list,fasta_dict):   
         #filter fasta sequences belonging to only bacteria
         filtered_bacterial_fasta_dictionary={}
@@ -65,7 +82,7 @@ class PrepareForMsa(object):
                 filtered_bacterial_fasta_dictionary[bacteria_name]=fasta_dict[protid_list][0]
         return filtered_bacterial_fasta_dictionary
     
-    def remove_species_level_redundancy(self,filtered_bacterial_fasta_dictionary):
+    def remove_species_level_redundancy(self,filtered_bacterial_fasta_dictionary,prefix):
         #pd.DataFrame.from_dict(filtered_bacterial_fasta_dictionary)
         filtered_bacterial_fasta_dataframe = pd.DataFrame.from_dict(filtered_bacterial_fasta_dictionary, orient = 'Index', columns=['Sequences'])
         filtered_bacterial_fasta_dataframe.index.name = 'Bacteria_name'
@@ -75,14 +92,22 @@ class PrepareForMsa(object):
 
         #remove duplicate rows
         filtered_bacterial_fasta_dataframe = filtered_bacterial_fasta_dataframe.drop_duplicates(subset='Genus_species', keep="first")
-        filtered_bacterial_fasta_dataframe['Genus_species'] = PREFIX + "_" + filtered_bacterial_fasta_dataframe['Genus_species']
+        filtered_bacterial_fasta_dataframe['Genus_species'] = prefix + "_" + filtered_bacterial_fasta_dataframe['Genus_species']
 
         return filtered_bacterial_fasta_dataframe
 
-    def write_filtered_fasta_file(self,filtered_bacterial_fasta_dataframe):
-        with open(WRITE,"w") as fh:
+    def write_filtered_fasta_file(self,filtered_bacterial_fasta_dataframe,write):
+        with open(write,"w") as fh:
             for ind in filtered_bacterial_fasta_dataframe.index:
                 fh.write(">%s\n%s\n"%(filtered_bacterial_fasta_dataframe['Genus_species'][ind], filtered_bacterial_fasta_dataframe['Sequences'][ind]))
+                
+    def write_merged_filtered_fasta_file(self,WRITE,MERGED_FASTA):
+        with open(MERGED_FASTA,"a+") as fh:
+            for filename in WRITE:
+                fr = open(filename,"r")
+                fh.write(fr.read())
+                fr.seek(0)
+                fr.close()
 
 if __name__=="__main__":
     
